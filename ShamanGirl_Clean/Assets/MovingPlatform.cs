ac01c2c4b1582710cbb64b36c73a5ee9af3bc3fb
Toni Steyskal <toni.steyskal@gmail.com>
2016-01-31 12:13:34 +0100

    Level2Update

---
+++ ShamanGirl_Clean/Assets/MovingPlatform.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.SetParent(transform);
+            collision.gameObject.GetComponent<Rigidbody>().velocity = gameObject.GetComponent<Rigidbody>().velocity;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            collision.gameObject.transform.SetParent(null);
+    }
+}
